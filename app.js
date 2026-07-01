014f9d9a59a8572be48af0e516499138aef42c20
dd60cm <dd60cm@gmail.com>
2026-07-01 13:17:36 +0900

    カレンダー表示

--- app.js
+++ app.js
@@ -6,6 +6,105 @@ const CONFIG = {
 
 };
 
+// カレンダー・コールバック関数
+
+async function loadEvents(info, successCallback, failureCallback) {
+
+    try {
+	    const auth = await authenticate();
+        const events = await getEvents(
+            info.startStr,
+            info.endStr,
+            auth
+        );
+
+/* ダミーデータ const events = [
+
+  { title: 'A', start: '2026-08-01' },
+
+  { title: 'B', start: '2026-08-02' },
+
+  { title: 'C', start: '2026-08-03' },
+
+  { title: 'C', start: '2026-08-04' },
+
+  { title: 'A', start: '2026-08-05' },
+
+  { title: 'B', start: '2026-08-06' },
+
+  { title: 'C', start: '2026-08-07' },
+
+  { title: 'C', start: '2026-08-08' },
+
+  { title: 'B', start: '2026-08-09' },
+
+  { title: 'C', start: '2026-08-10' },
+
+  { title: 'C', start: '2026-08-11' },
+
+  { title: 'C', start: '2026-08-12' }
+
+];*/
+        successCallback(events);
+
+    } catch (e) {
+
+        console.error('loadEvents:', e);
+    }
+}
+
+async function getEvents(start, end, auth = {}) {
+
+    const payload ={
+
+        ...auth,
+        
+        start,
+        
+        end
+    };
+    
+    const result = await post("GET_EVENTS", payload);
+    
+    if (!result.success) {
+        
+        alert(result.message);
+        
+        return [];
+    }
+    
+    return result.events;
+}
+
+// 認証処理
+
+async function authenticate() {
+
+    const sessionToken = sessionStorage.getItem("sessionToken");
+
+    if (sessionToken) {
+
+        return {
+            sessionToken
+        };
+    }
+
+    const accessToken = await getAccessToken();
+
+    const result = await post("AUTH", {
+        accessToken
+    });
+
+    sessionStorage.setItem(
+        "sessionToken",
+        result.sessionToken
+    );
+
+    return {
+        sessionToken: result.sessionToken
+    };
+}
+
 // アクセストークンの取得
 
 async function getAccessToken() {
@@ -100,26 +199,59 @@ async function post(action, body = {} ) {
     }
 }
 
+// テスト用・データ取得と反映のための別関数
+async function loadAndReflectEvents(calendar) {
+    try {
+        const auth = await authenticate();
+        const result = await getEvents('2026-06-01', '2026-09-01', auth);
+        
+        calendar.addEventSource(result);
+    
+    } catch (error) {
+        console.error('後読み込みデータの設定に失敗しました:', error);
+    }
+}
+
 // メイン処理
 
 async function main() {
 
   try {
 
-        const accessToken = await getAccessToken();
+        // const auth = await authenticate();
 
-        const result =
+        // const result = await getEvents('2026-07-01', '2026-08-01', auth);
+        
+        
+            
+        const calendarEl = document.getElementById('calendar');
+            
+        /* events コールバック方式
+        const calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            locale: 'ja',
+            events: loadEvents
+        });*/
+        
+        // 1. 先に空っぽのカレンダーを描画しておく
+        const calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            locale: 'ja'
+        });
+        calendar.render();
+        
+        // 2. データを非同期で取得して後から追加する関数を実行
+        loadAndReflectEvents(calendar);
 
-            await post("count", { accessToken });
     
-    document.getElementById("result").textContent = JSON.stringify(result);
+    // document.getElementById("result").textContent = JSON.stringify(result);
 
 
   } catch(err) {
 
     console.log(err);
 
-    document.getElementById("result").textContent = JSON.stringify(err);
+    document.getElementById("calendar").textContent = JSON.stringify(err);
   }
 }
 
